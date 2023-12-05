884b5f0a5b76c582e38b4f99c37931001126670e
MarioHernanCoria <mariocoria025@gmail.com>
2023-12-04 21:25:38 -0300

    Agregar archivos de proyecto.

---
+++ WebAppMerck/Models/FormularioData.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppMerck.Models
+{
+    public class FormularioData
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "La edad actual es requerida")]
+        public int EdadActual { get; set; }
+        [Required(ErrorMessage = "La edad de la primera menstruación es requerida")]
+        public int EdadPrimeraMenstruacion { get; set; }
+        public string NivelFertilidad { get; set; } 
+    }
+}
